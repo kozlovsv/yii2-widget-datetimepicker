152141ebf381d71fa2b52522d98efd41cb8e1a21
kozlovsv <kozlovsv78@gmail.com>
2016-11-18 18:23:58 +0500

    Add files via upload

---
+++ DateTimePicker.php
@@ -0,0 +1,157 @@
+<?php
+
+namespace kozlovsv\datepicker;
+
+use Yii;
+use yii\helpers\Html;
+use yii\helpers\Json;
+use yii\widgets\MaskedInput;
+
+/**
+ * @author Ilya Norkin
+ */
+class DateTimePicker extends MaskedInput
+{
+    /**
+     * @var string
+     */
+    public $mask = 'd.m.y';
+
+    /**
+     * @var string the language to use
+     */
+    public $language;
+
+    /**
+     * @var array the options for the Bootstrap DatePicker plugin.
+     */
+    public $clientOptions = [];
+
+    /**
+     * @var array the event handlers for the underlying Bootstrap Switch 3 input JS plugin.
+     */
+    public $clientEvents = [];
+
+    /**
+     * @var string the size of the input ('lg', 'md', 'sm', 'xs')
+     */
+    public $size;
+
+    /**
+     * @var array HTML attributes to render on the container if its used as a component.
+     */
+    public $containerOptions = [];
+
+    /**
+     * @var string the template to render the input. By default, renders as a component, you can render a simple
+     * input field without pickup and/or reset buttons by modifying the template to `{input}`. `{button}` must exist for
+     * a component type of datepicker. The following template is invalid `{input}{reset}` and will be treated as `{input}`
+     */
+    public $template = "{input}";
+
+    /**
+     * @var string the icon to use on the reset button
+     */
+    public $resetButtonIcon = 'glyphicon glyphicon-remove';
+
+    /**
+     * @var string the icon to use on the pickup button. Defaults to `glyphicon-th`. Other uses are `glyphicon-time` and
+     * `glyphicon-calendar`.
+     */
+    public $pickButtonIcon = 'glyphicon glyphicon-th';
+
+    /**
+     * @var bool whether to render the input as an inline calendar
+     */
+    public $inline = false;
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+
+        Html::addCssClass($this->containerOptions, 'input-group date');
+        Html::addCssClass($this->options, 'form-control');
+        if ($this->size !== null) {
+            $size = 'input-' . $this->size;
+            Html::addCssClass($this->options, $size);
+            Html::addCssClass($this->containerOptions, $size);
+        }
+        if ($this->inline) {
+            $this->clientOptions['linkField'] = $this->options['id'];
+            Html::removeCssClass($this->containerOptions, 'date');
+            Html::removeCssClass($this->containerOptions, 'input-group');
+            Html::addCssClass($this->options, 'text-center');
+        }
+
+        $value = $this->model{$this->attribute};
+        if ($this->model{$this->attribute} != null) {
+            $format = isset($this->clientOptions['pickTime']) && $this->clientOptions['pickTime'] === false ? 'date' : 'datetime';
+            $this->model{$this->attribute} = Yii::$app->formatter->format($value, $format);
+        }
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function run()
+    {
+        $input = MaskedInput::widget([
+            'mask' => $this->mask,
+            'model' => $this->model,
+            'attribute' => $this->attribute,
+            'options' => $this->options
+        ]);
+
+        if (!$this->inline) {
+            $resetIcon = Html::tag('span', '', ['class' => $this->resetButtonIcon]);
+            $pickIcon = Html::tag('span', '', ['class' => $this->pickButtonIcon]);
+            $resetAddon = Html::tag('span', $resetIcon, ['class' => 'input-group-addon']);
+            $pickerAddon = Html::tag('span', $pickIcon, ['class' => 'input-group-addon']);
+        } else {
+            $resetAddon = $pickerAddon = '';
+        }
+
+        if (strpos($this->template, '{button}') !== false || $this->inline) {
+            $input = Html::tag(
+                'div', strtr($this->template, ['{input}' => $input, '{reset}' => $resetAddon, '{button}' => $pickerAddon]), $this->containerOptions
+            );
+        }
+        echo $input;
+        $this->registerClientScript();
+    }
+
+    /**
+     * Registers required script for the plugin to work as a DateTimePicker
+     */
+    public function registerClientScript()
+    {
+        $js = [];
+        $view = $this->getView();
+        DateTimePickerAsset::register($view);
+        $id = $this->options['id'];
+        $selector = ";jQuery('#$id')";
+
+        if (strpos($this->template, '{button}') !== false || $this->inline) {
+            $selector .= ".parent()";
+        }
+
+        $options = !empty($this->clientOptions) ? Json::encode($this->clientOptions) : '';
+
+        $js[] = "$selector.datetimepicker($options);";
+
+        if ($this->inline) {
+            $js[] = "$selector.find('.datetimepicker-inline').addClass('center-block');";
+            $js[] = "$selector.find('table.table-condensed').attr('align','center').css('margin','auto');";
+        }
+        if (!empty($this->clientEvents)) {
+            foreach ($this->clientEvents as $event => $handler) {
+                $js[] = "$selector.on('$event', $handler);";
+            }
+        }
+        $view->registerJs(implode("\n", $js), $view::POS_READY);
+    }
+
+}
---
+++ DateTimePickerAsset.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace kozlovsv\datepicker;
+
+use yii\web\AssetBundle;
+
+/**
+ * @author Ilya Norkin
+ */
+class DateTimePickerAsset extends AssetBundle
+{
+
+    public $sourcePath = __DIR__ . '/assets';
+
+    public function init()
+    {
+        if (empty(YII_DEBUG)) {
+            $this->js[] = 'js/moment-with-locales.min.js';
+            $this->js[] = 'js/bootstrap-datetimepicker.min.js';
+            $this->css[] = 'css/bootstrap-datetimepicker.min.css';
+        } else {
+            $this->js[] = 'js/moment-with-locales.min.js';
+            $this->js[] = 'js/bootstrap-datetimepicker.js';
+            $this->css[] = 'css/bootstrap-datetimepicker.css';
+        }
+    }
+
+    public $depends = [
+        'yii\web\JqueryAsset',
+        'yii\bootstrap\BootstrapAsset',
+    ];
+
+}
